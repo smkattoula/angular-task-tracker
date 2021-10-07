83e6d6f577a11aafe16f0c3e279fe87c1a5a8827
Shaker Kattoula <smkattoula@gmail.com>
2021-10-07 13:29:30 -0700

    initial commit, create header and button components, add global css styling, render header and button in UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
